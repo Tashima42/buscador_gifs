2bf1e195808b4b5662baaacef801806dd14531cc
Pedro Tashima <23709916+Tashima42@users.noreply.github.com>
2020-06-26 00:26:51 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:http/http.dart';
+
+void main(){
+  runApp: runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+ 
